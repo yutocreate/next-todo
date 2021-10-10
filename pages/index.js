4df4d6c59185afcbc75e0ba3bca7998a8e13de18
idenyuto <1otuy26@gmail.com>
2021-10-10 23:21:12 +0900

    テキストフォームと削除機能の実装

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import React, { useState, useEffect, memo } from "react";
 import { db } from "../src/firebase";
 import "firebase/firestore";
-import React, { useState, useEffect } from "react";
-import {TextField} from "@material-ui/core";
+import Link from "next/link";
+import styled from "styled-components";
 
-export default function Home() {
-  const [todo, setTodos] = useState('')
+let nowId = 0;
+const Home = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
 
-  const textOnChange = (e) => {
-    setTodos(e.target.value)
-  }
+  console.log(todos);
+
+  const change = (e) => {
+    setTodo(e.target.value);
+  };
 
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
@@ -22,16 +25,68 @@ export default function Home() {
     return () => unSub();
   }, []);
 
+  const onSubmit = (todoText) => {
+    const newTodo = {
+      id: nowId,
+      text: todoText,
+      editing: false,
+      completed: false,
+    };
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+    nowId += 1;
+  };
+
+  const handleSubmit = (e) => {
+    if (todo === "") return;
+    e.preventDefault();
+    onSubmit(todo);
+    setTodo("");
+  };
+
+const deleteTodo = (id) => {
+  const newTodos = todos.filter((todo) => todo.id !== id)
+  setTodos(newTodos)
+}
+
   return (
     <>
-      <TextField autoFocus value={todo} onChange={textOnChange}/>
-      <div className={styles.container}>
-        <ol>
-          {tasks.map((task) => (
-            <li key={task.id}>{task.title}</li>
-          ))}
-        </ol>
+      <h1>NextTodoアプリ</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="todoを追加"
+          value={todo}
+          onChange={change}
+        />
+      </form>
+
+      {todos.map(({ id, text }) => {
+        return (
+          <>
+            <div style={{ display: "flex" }}>
+              <p key={id}>{text}</p>
+              <button onClick={() => deleteTodo(id)}>削除</button>
+            </div>
+          </>
+        );
+      })}
+
+      <div>
+        {tasks.map((task) => (
+          <Link href="/page1" key={task.id}>
+            <a style={{ margin: "4px" }}>{task.title}</a>
+          </Link>
+        ))}
       </div>
     </>
   );
-}
+};
+
+export default Home;
+
+const a = styled.a`
+  display: flex;
+  margin: 4px;
+  cursor: pointer;
+`;
